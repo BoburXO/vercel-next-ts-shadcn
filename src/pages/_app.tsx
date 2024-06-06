c910158bd98b8a9fe149bbbccea95b3051c49f03
BoburXO <boburhatamov7@gmail.com>
2024-06-06 14:24:12 +0500

    create: project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/layout/Footer/Footer";
+import Header from "@/layout/Header/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+}
